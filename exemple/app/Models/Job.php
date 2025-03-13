2210c7d102c3622d2e2a24429b72487c9afa63ab
Adrien <adrien@p3f.ca>
2025-03-13 10:51:29 -0400

    model ELOQUENT

--- exemple/app/Models/Job.php
+++ exemple/app/Models/Job.php
@@ -2,40 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
-{
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Teacher',
-                'location' => 'Remote',
-                'salary' => '$40,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Software Engineer',
-                'location' => 'Remote',
-                'salary' => '$100,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Data Analyst',
-                'location' => 'Regina',
-                'salary' => '$80,000',
-            ],
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        }
-        return $job;
-    }
-}
+class Job extends Model {}
