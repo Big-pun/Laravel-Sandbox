4cbb5c85070ae8541dc570642ae1fe7511f1d6c3
Adrien <adrien@p3f.ca>
2025-04-01 09:22:56 -0400

    edit ok

--- exemple/resources/views/jobs/edit.blade.php
+++ exemple/resources/views/jobs/edit.blade.php
@@ -41,7 +41,7 @@
                                        name="remote" 
                                        id="remote"
                                        x-model="isRemote"
-                                       value="1"
+
                                        {{ $job->location === 'Remote' ? 'checked' : '' }}
                                        class="rounded-md border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                 <span class="ml-2">Remote Position</span>
